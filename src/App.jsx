5d0b4fea75f45f2fdab9189ca76f9c68766809c6
far1h <farihmuhammad45@gmail.com>
2024-10-18 11:00:29 +0700

    Render Multi-Dimensional Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import Gameboard from "./components/GameBoard";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="X"></Player>
           <Player initialName="Player 2" symbol="O"></Player>
         </ol>
-        GAME BOARD
+        <Gameboard></Gameboard>
       </div>
       LOG
     </main>
