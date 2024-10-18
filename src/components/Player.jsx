f5781ecd7cafaed00d59aa8e34a644ae7f81342a
far1h <farihmuhammad45@gmail.com>
2024-10-18 08:27:04 +0700

    Add conditional content for buttons

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,19 +3,21 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
   let playerName = <span className="player-name">{name}</span>;
+  let buttonName = "Edit";
   if (isEditing) {
-    playerName = <input type="text" required />;
+      playerName = <input type="text" required value={name}/>;
+      buttonName = "Save";
   }
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
-  return (
+  return  (
     <li>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{buttonName}</button>
     </li>
   );
 }
